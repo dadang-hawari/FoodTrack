bade9a67c95159f6a827024de7bd4f16220e37b8
dadanghawari <muhammaddadanghawari@gmail.com>
2024-05-14 12:03:44 +0800

    add new feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
   const trivia = useSelector((state) => state?.food?.trivia);
 
   useEffect(() => {
-    // dispatch(getTrivia());
+    dispatch(getTrivia());
     checkLocationState(location, navigate);
   }, []);
 
